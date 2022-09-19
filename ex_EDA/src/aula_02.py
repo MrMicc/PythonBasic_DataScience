bb233297ef89c38e25c4557e348917b24a171de1
Micci <luiz.miccieli@gmail.com>
2022-09-18 23:31:18 -0300

    Aula 02 - Adicionada a funções que olham para a quantidade de valores repetidos em uma variavel de um dataframe e os valores que aparecem em uma determinada variavel

--- ex_EDA/src/aula_02.py
+++ ex_EDA/src/aula_02.py
@@ -31,7 +31,49 @@ def carrega_dados(uri: str='', sep = ';') -> pd.DataFrame:
     return dados
 
 
+def valores_unicos(data_frame: pd.DataFrame, variavel: str= 'quality'):
+    '''função responsável por retornar os valores únicos de uma determinada variavel em um DataFrame
+        
+        Parameters
+        ----------
+        data_frame: DataFrame 
+        varavel: str -> quality
+        
+        Return
+        ---------
+        array 
+    '''
+
+    return  data_frame[variavel].unique()
+
+
+
+
+
+
+def frequencia(data_frame: pd.DataFrame, variavel = 'quality'):
+    '''Função responsável por retonar a frequencia de valores de uma determinada variavel em um DataFrame
+
+        Parameters
+        ----------
+        data_frame: DataFrame
+        variavel: str -> quality
+
+        Return
+        --------
+        Panda Serie
+    '''
+    return data_frame[variavel].value_counts()     
+
 
 if __name__ == '__main__':
 
-    carrega_dados(URI) 
+    dados = carrega_dados(URI)
+    print(dados.columns)
+
+
+
+    print('Analise quai são os votos de qualidade dos vinhos:')
+    print(valores_unicos(data_frame = dados, variavel = 'quality'))
+    print('Frequencia de que cada voto foi dado')
+    print(frequencia(data_frame=dados, variavel= 'quality')) 
--- ex_EDA/test/aula_02_test.py
+++ ex_EDA/test/aula_02_test.py
@@ -1,5 +1,6 @@
 import pandas as pd  
-from src.aula_02 import carrega_dados 
+import numpy as np 
+from src.aula_02 import carrega_dados, valores_unicos, frequencia
 
 URI = 'https://raw.githubusercontent.com/dphi-official/Datasets/master/Wine_Dataset/winequality-red.csv'
 
@@ -10,4 +11,24 @@ def test_carrega_dados():
 
     assert quero == recebi 
 
+def test_valores_unicos():
+    dados = carrega_dados(URI)
+
+    recebi = valores_unicos(data_frame= dados, variavel='quality').tolist()
+    recebi.sort()
+    quero = [3,4,5,6,7,8]
+
+    assert  recebi == quero
+
+
+def test_frequencia():
+    dados = carrega_dados(URI)
+
+    recebi = frequencia(data_frame = dados, variavel = 'quality')
+    
+    d = {5: 681, 6: 638,  7:199, 4:53, 8:18, 3:10}
+    quero = pd.Series(data=d, index= [3,4,5,6,7,8], name='quality')
+    quero = quero.sort_values(ascending=False) 
+
+    assert recebi.equals(quero) 
 
