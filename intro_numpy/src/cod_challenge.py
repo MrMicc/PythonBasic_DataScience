64665e5a2dbb1e6a2e0015d9a8d6ba13b49c15da
Micci <luiz.miccieli@gmail.com>
2022-10-04 22:46:02 -0400

    Aula1 Numpy challange 3 - criação de um array customizado a partir de inicio, fim, intervalo de numero e casas decimais

--- intro_numpy/src/cod_challenge.py
+++ intro_numpy/src/cod_challenge.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-#exercio 1 
+#exercicio 1 
 def array_impares(quantidade: int = 1) -> np.ndarray:
 
     num = 1 
@@ -23,3 +23,14 @@ def array_dimensao(lista: list= ['1']) -> int:
     arr = np.array(lista)
     return arr.ndim 
 
+
+#exercicio 3 
+def array_aleatorio(comeca_em: int = 1, termina_em: int = 10, qtd_intervalos = 5, qtd_casas_decimais_nos_intervalos = 8):
+
+    arr =  np.linspace(start = comeca_em, stop = termina_em, num = qtd_intervalos)
+    arr_arrendondado = [] 
+
+    for x in arr: 
+        arr_arrendondado = np.append(arr_arrendondado, round(x, qtd_casas_decimais_nos_intervalos))
+
+    return arr_arrendondado 
