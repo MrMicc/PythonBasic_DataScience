f16d1ed0bb077ea6f45b2b21e7e119f44a8484fb
Micci <luiz.miccieli@gmail.com>
2022-11-13 18:48:43 -0300

    Aula 2 Pandas handon 1 => Retorno de tx_crime a partir de um indice

--- intro_pandas/src/hands_on.py
+++ intro_pandas/src/hands_on.py
@@ -1,10 +1,8 @@
 import pandas as pd
 
 
-def carrega_dados(uri: str, sep = ',') -> pd.Series:
+def carrega_dados(uri: str, sep=',') -> pd.Series:
     return pd.read_csv(uri, sep=sep)
-
-
 
 
 def retorna_formato(ds: pd.Series):
@@ -12,14 +10,19 @@ def retorna_formato(ds: pd.Series):
     return ds.shape
 
 
+def tx_crime_no_indice(uri: str, index: int) -> float:
+
+    df = carrega_dados(uri=uri)
+
+    return df.loc[index, ['crime_rate']]
+
 
 if __name__ == '__main__':
-    
+
     uri = 'https://raw.githubusercontent.com/dphi-official/Datasets/master/Standard_Metropolitan_Areas_Data-data.csv'
 
     df = carrega_dados(uri)
 
-    print(retorna_formato(df))
-
-
     print(df.info())
+
+    print(tx_crime_no_indice(uri=uri, index=9))
--- intro_pandas/test/hands_on_test.py
+++ intro_pandas/test/hands_on_test.py
@@ -4,12 +4,13 @@ from src.hands_on import *
 
 URI = 'https://raw.githubusercontent.com/dphi-official/Datasets/master/Standard_Metropolitan_Areas_Data-data.csv'
 
+
 def test_carrega_dados():
 
     recebi = carrega_dados(URI, ',')
     quero = pd.read_csv(URI)
 
-    assert  (recebi == quero).all
+    assert (recebi == quero).all
 
 
 def test_retorna_formato():
@@ -18,5 +19,14 @@ def test_retorna_formato():
 
     quero = ds.shape
 
-
     assert recebi == quero
+
+
+# Exercicio HandsOn2 - 1
+def test_tx_crime_no_indice():
+    df = carrega_dados(URI)
+    recebi = tx_crime_no_indice(URI, index=9)
+
+    quero = 55.30
+
+    assert recebi[0] == quero
